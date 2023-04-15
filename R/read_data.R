acd0c78ddd0adc92e86a28b75b15986ca0e10fae
max607 <maxschneider2000@proton.me>
2023-04-15 10:54:16 +0200

    persons

--- R/read_data.R
+++ R/read_data.R
@@ -1,4 +1,9 @@
 # prep
 library(data.table)
-dt_firms <- fread("data/DataFest2023/firmen.csv")
+dt_firms <- fread("data/DataFest2023/firmen.csv", nrows = 1000)
+dt_persons <- fread("data/DataFest2023/personen.csv")
+
+# letter box
+# number of employes
+# too many companies in one specific point
 
