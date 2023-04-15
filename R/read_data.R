7b8580e8a29aa7a6d71aaff250d98f9657a510aa
schade-rebekka <woodstone@posteo.de>
2023-04-15 12:56:55 +0200

    first framework for clustering

--- R/read_data.R
+++ R/read_data.R
@@ -1,6 +1,5 @@
 # Preparation --------------------------------------------------------------------------------------
 
-library(data.table)
 dt_firms <- fread("data/DataFest2023/firmen.csv", nrows = 1000)
 dt_persons <- fread("data/DataFest2023/personen.csv", nrows = 1000)
 dt_persons_firms <- fread("data/DataFest2023/relationen_person_firma.csv", nrows = 1000)
--- main.R
+++ main.R
@@ -3,8 +3,11 @@
 library(magrittr)
 library(data.table)
 library(ggplot2); theme_set(theme_bw())
+library(mlr3)
+library(mlr3cluster)
 
 # Scripts ------------------------------------------------------------------------------------------
 
 source("R/read_data.R")
+source("R/clustering.R")
 
