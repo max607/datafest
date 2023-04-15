ece0e6bee07217a2d24617a3deb104ee8d14c155
max607 <maxschneider2000@proton.me>
2023-04-15 11:06:45 +0200

    read 3 datasets

--- R/read_data.R
+++ R/read_data.R
@@ -1,7 +1,8 @@
 # prep
 library(data.table)
 dt_firms <- fread("data/DataFest2023/firmen.csv", nrows = 1000)
-dt_persons <- fread("data/DataFest2023/personen.csv")
+dt_persons <- fread("data/DataFest2023/personen.csv", nrows = 1000)
+dt_persons_firms <- fread("data/DataFest2023/relationen_person_firma.csv", nrows = 1000)
 
 # letter box
 # number of employes
